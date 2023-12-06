4c7a371821bbaae7d33c6b04c12de069097e0880
Nyamadi Atsu <devbrainy98@gmail.com>
2023-12-06 03:24:48 +0100

    fixed document not defined error and infinite preloading error

--- src/app/context/NavProvider.tsx
+++ src/app/context/NavProvider.tsx
@@ -30,6 +30,7 @@ const NavProvider = ({ children }: NavContextProps) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const pathname = usePathname();
   const pathnameRef = useRef<string>();
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
     if (pathnameRef.current !== undefined && pathnameRef.current !== pathname) {
@@ -38,25 +39,35 @@ const NavProvider = ({ children }: NavContextProps) => {
     pathnameRef.current = pathname;
   }, [pathname]);
 
+  useEffect(() => {
+    setShowLoader(true);
+
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 3000);
+
+    return () => {
+      setShowLoader(false);
+    };
+  }, []);
+
   const values = {
     isNavOpen,
     setIsNavOpen,
   };
-  return (
-    <NavContext.Provider value={values}>
-      <>
-        {typeof window !== "undefined" && (
-          <Sugar
-            color={"#ffff"}
-            background={
-              "linear-gradient(90deg, rgba(9,7,43,1) 30%, rgba(2,16,49,1) 70%)"
-            }
-          />
-        )}
-        {children}
-      </>
-    </NavContext.Provider>
-  );
+
+  if (typeof window !== "undefined" && showLoader) {
+    return (
+      <Sugar
+        color={"#ffff"}
+        background={
+          "linear-gradient(90deg, rgba(9,7,43,1) 30%, rgba(2,16,49,1) 70%)"
+        }
+      />
+    );
+  }
+
+  return <NavContext.Provider value={values}>{children}</NavContext.Provider>;
 };
 
 export default NavProvider;
