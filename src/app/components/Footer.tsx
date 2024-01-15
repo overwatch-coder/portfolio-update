e3ad64bfa83bea0143075f533898c638768f15bf
Nyamadi Atsu <devbrainy98@gmail.com>
2024-01-15 20:23:45 +0100

    changed year in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="w-full mx-auto text-center">
         <hr className="border border-slate-900 my-3" />
         <p className="text-[10px] md:text-sm flex items-center justify-between px-2 md:px-10">
-          <span>Copyright © {new Date().getFullYear()} Overwatch Coder</span>
+          <span>Copyright © 2023 - {new Date().getFullYear()} Overwatch Coder</span>
           <span>Privacy Policy | All Rights Reserved</span>
         </p>
       </div>
